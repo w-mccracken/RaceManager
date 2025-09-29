7c04b3603f276aeb55d7e0a607666b276ea949b4
w-mccracken <wmmccrac@asu.edu>
2025-09-28 20:45:08 -0400

    Update StripePayment.java

--- RaceManager/src/model/payment/StripePayment.java
+++ RaceManager/src/model/payment/StripePayment.java
@@ -4,10 +4,9 @@ import model.User;
 
 public class StripePayment implements PaymentStrategy {
     @Override
-    public boolean pay(double amount, User payer, String reference, String details) {
-        System.out.printf("Paid $%.2f for %s via Stripe (%s)%n",
-                amount, reference, details);
+    public boolean pay(double amount, String payerName, String reference, String details) {
+        System.out.printf("Paid $%.2f for %s via Stripe (%s)%n", amount, reference, details);
+
         return true;
     }
 }
-
